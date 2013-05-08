7afb41ea1602ccac8fae0575921098c278476c00
Dylan <woythadc194@ubuntu.(none)>
2013-05-08 00:02:41 -0400

    Changed arrays to Integers for easier comparing later on.

--- PokerHand.java
+++ PokerHand.java
@@ -1,50 +1,64 @@
 /*
  * Author: Dylan Woythal
  */
- 
+import java.util.Scanner; 
 public class PokerHand{
 
     private String hand;
-    private String [] ary;
+    private int [] ary;
     
     public PokerHand(String hand){
         this.hand = hand;
         this.ary = getArray();
     }
     
-    private String[] getArray(){
-        String [] temp = new String[5];
+    private int [] getArray(){
+        int [] temp = new int[5];
         Scanner sc = new Scanner(hand);
         int counter = 0;
         while(sc.hasNext()){
-            temp[counter] = sc.next();
+            int x = 0;
+            char c = sc.next().charAt(0);
+            try{
+                x = Integer.parseInt("" + c);
+            } catch(Exception e){
+                if(c=='J')
+                    x = 11;
+                else if(c=='Q')
+                    x = 12;
+                else if(c=='K')
+                    x = 13;
+                else if(c=='A')
+                    x = 14;
+            }
+            temp[counter] = x;
             counter++;
         }
         return temp;
     }
     
     public String rank(){
-        String rank = "";
+        int rank = 0;
         if(royalFlush())
-            rank += "1 ";
+            rank = 1;
         else if(straightFlush())
-            rank += "2 ";
+            rank = 2;
         else if(fourOfAKind())
-            rank += "3 ";
+            rank = 3;
         else if(fullHouse())
-            rank += "4 ";
+            rank = 4;
         else if(flush())
-            rank += "5 ";
+            rank = 5;
         else if(straight())
-            rank += "6 ";
+            rank = 6;
         else if(threeOfAKind())
-            rank += "7 ";
+            rank = 7;
         else if(twoPair())
-            rank += "8 ";
+            rank = 8;
         else if(onePair())
-            rank += "9 ";
+            rank = 9;
         else
-            rank += "10 ";
+            rank = 10;
     }
     
     public String toString(){
