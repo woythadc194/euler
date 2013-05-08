3495e3c2ea5a95f6f4bddbe5a6280035a2e410b0
Dylan <woythadc194@ubuntu.(none)>
2013-05-07 20:23:28 -0400

    Pokerhand class for evaluating 5 card hands for euler and other possible future games

---
+++ PokerHand.java
@@ -0,0 +1,16 @@
+/*
+ * Author: Dylan Woythal
+ */
+ 
+public class PokerHand{
+
+    private String hand;
+    
+    public PokerHand(String hand){
+        this.hand = hand;
+    }
+    
+    public String toString(){
+        return hand;
+    }
+}
